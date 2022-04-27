8b09c6139f72edbeb3508a6f7ef896776667de46
Nizor <ogbezuodenizor@yahoo.com>
2022-04-27 12:57:37 +0100

    test .gitignore

--- api/main.py
+++ api/main.py
@@ -1,15 +1,20 @@
 #importing libraries
 from fastapi import FastAPI, File, UploadFile
+import uvicorn
 
 #instance
 app = FastAPI()
 
-@app.get("/ping") #endpoint
+@app.get("/ping") #test endpoint
 async def ping():
     return "Hello"
 
-@app.post("/predict") #endpoint
+
+@app.post("/predict")
 async def predict(
-    file: UploadFile = File(...)
+    file:UploadFile = File(...)
 ):
     pass
+
+if __name__ == "__main__":
+    uvicorn.run(app,host='localhost', port=8000)
